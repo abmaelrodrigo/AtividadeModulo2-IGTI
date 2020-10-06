92ad7ca14fca140401489788105f294c45b1511f
Abmael Rodrigo <abmaelrodrigo@outlook.com>
2020-10-05 22:26:56 -0300

    mudança de async para async

--- script.js
+++ script.js
@@ -1,67 +1,45 @@
-import { promises as fs } from 'fs';
-//init();
-let states = new Object();
-let cities = new Object();
-let arrayCidades = [];
-let cidadesEmJson = {
-  cidades: arrayCidades,
-};
+import * as fs from 'fs';
 
-writeReadJson();
-
-async function writeReadJson() {
-  try {
-    states = JSON.parse(
-      await fs.readFile('./cidades-estados-brasil-json/Estados.json')
-    );
-    cities = JSON.parse(
-      await fs.readFile('./cidades-estados-brasil-json/Cidades.json')
-    );
-
-    /*states.map((estados) => {
-      const { ID, Sigla, Nome } = estados;
-      return {
-        ID: ID,
-        Sigla: Sigla,
-        Nome: Nome,
-      };
-    });*/
-
-    states.forEach((estados) => {
-      const { ID, Sigla, Nome } = estados;
-
-      cities.forEach((cidades) => {
-        const { ID, Nome, Estado } = cidades;
-        if (estados.ID === cidades.Estado) {
-          //console.log('oi');
-          //let arrayCidadesEspecificas =
-          createJsonFile(estados, cidades);
-        }
-      });
-      //let id = ID;
-      // await fs.writeFile('teste.json', JSON.stringify(jsonObj));
-
-      //console.log(Sigla);
-    });
-  } catch (err) {
-    console.log(err);
-  }
+function readFile(filename) {
+  return fs.readFileSync(filename, 'UTF-8');
 }
 
-async function createJsonFile(estados, cities) {
-  try {
-    let nameOfFile = estados.Sigla + '.json';
-
-    if (estados.ID === '3') {
-      await fs.writeFile(nameOfFile, JSON.stringify(cidadesEmJson));
-      const data = JSON.parse(await fs.readFile(nameOfFile));
-      data.cidades.push(cities.Nome);
-      //arrayCidades.push(cities.Nome);
-      await fs.writeFile(nameOfFile, JSON.stringify(data));
-      //console.log(JSON.stringify(data));
-    }
-  } catch (err) {
-    console.log(err);
-  }
-  //console.log(cidadesEmJson);
+function saveFile(filename, content) {
+  fs.writeFileSync(filename, content, 'UTF-8');
 }
+
+function jsonToObject(string) {
+  return JSON.parse(string);
+}
+
+//const states = JSON.parse(readFile('Estados.json'));
+//const cities = readFile('Cidades.json');
+//console.log(cities);
+
+saveReadFiles();
+function saveReadFiles() {
+  const states = JSON.parse(
+    readFile('./cidades-estados-brasil-json/Estados.json')
+  );
+  const cities = JSON.parse(
+    readFile('./cidades-estados-brasil-json/Cidades.json')
+  );
+
+  states.forEach((state) => {
+    const { ID, Sigla, Nome } = state;
+    const citiesOfState = filterCityByState(ID, cities);
+    saveFile(`${state.Sigla}.json`, JSON.stringify(citiesOfState, null, 4));
+  });
+}
+
+function filterCityByState(stateID, cities) {
+  return cities.filter((city) => city.Estado === stateID);
+}
+
+function numberOfCities(uf) {
+  const number = jsonToObject(readFile(`${uf}.json`)).length;
+  return number;
+}
+
+const t = numberOfCities('MG');
+console.log(t);
